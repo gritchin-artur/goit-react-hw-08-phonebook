aed2b0e186776b16148b1af81d519b1c2b9ac10c
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-09-09 14:38:06 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Layout } from "./Layout/Layout";
-import { Suspense, lazy, useEffect } from "react";
+import { Suspense, lazy } from "react";
 import PrivateRoute from "../Routes/PrivatRoute/PrivateRoute";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import authSelectors from "../redux/auth/auth-selectors";
 import { Toaster } from "react-hot-toast";
 import PublicRoute from "../Routes/PublicRoute/PublicRoute";
-import authOperations from "../redux/auth/auth-operations";
+// import authOperations from "../redux/auth/auth-operations";
 
 const Home = lazy(() => import("../pages/Home/Home"));
 const PageContacts = lazy(() => import("../pages/PageContacts/PageContacts"));
@@ -14,7 +14,7 @@ const LogIn = lazy(() => import("../pages/LogIn/LogIn"));
 const SingUp = lazy(() => import("../pages/SingUp/SingUp"));
 
 export function App() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
 
   // useEffect(() => {
